d258987146b038d6b3b912c8705223aaa3438e82
shikhat3190-cloud <shikha.t3190@gmail.com>
2025-12-09 11:05:27 +0530

    Implement PDF loading functionality
    Added functionality to load PDF documents using LangChain.

--- part2_rag_agent.py
+++ part2_rag_agent.py
@@ -5,3 +5,29 @@
                "pypdf" \
                "langchain-text-splitters" \
                "chromadb"
+
+# Get API keys from environment variables
+from google.colab import userdata
+import google.generativeai as genai
+GEMINI_API_KEY = userdata.get("GOOGLE_API_KEY")
+if not GEMINI_API_KEY:
+    print("Warning: GEMINI_API_KEY not found. Gemini model will not run.")
+
+
+## Loading PDF
+from langchain_community.document_loaders import PyPDFLoader
+
+def load_documents(pdf_paths: list):
+    """Loads PDFs and returns LangChain Document objects."""
+    docs = []
+    for path in pdf_paths:
+        loader = PyPDFLoader(path)
+        docs.extend(loader.load())
+    return docs
+
+# Example usage:
+#pdf_files = ["/content/Data_Analysis_FAQ_20_QA.pdf"]  # upload PDF in Colab first
+pdf_files = ["/content/oci-faq.pdf"]
+documents = load_documents(pdf_files)
+print(f"Loaded documents: {len(documents)} pages")
+print(documents)
