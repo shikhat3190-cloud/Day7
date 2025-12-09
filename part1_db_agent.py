e1a7595b77736eb1ef0522c5baec5b9cb3b8c2bd
shikhat3190-cloud <shikha.t3190@gmail.com>
2025-12-09 09:50:43 +0530

    Update part1_db_agent.py

--- part1_db_agent.py
+++ part1_db_agent.py
@@ -1,3 +1,6 @@
+#model="gemini-2.5-flash-lite",
+#model="gemini-2.5-flash"
+
 !pip install -U "langchain>=0.3.12" \
                "langchain-core>=0.3.30" \
                "langchain-community>=0.3.12" \
