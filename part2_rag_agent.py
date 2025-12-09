37285523dd2b608c1dc82d0199e8124d73b4e8c4
shikhat3190-cloud <shikha.t3190@gmail.com>
2025-12-09 11:01:35 +0530

    Create part2_rag_agent.py

---
+++ part2_rag_agent.py
@@ -0,0 +1,7 @@
+!pip install -U "langchain>=0.3.12" \
+               "langchain-core>=0.3.30" \
+               "langchain-community>=0.3.12" \
+               "langchain-google-genai>=2.0.0" \
+               "pypdf" \
+               "langchain-text-splitters" \
+               "chromadb"
